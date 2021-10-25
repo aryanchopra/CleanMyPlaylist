4c9ed284d1bcce28b473d76dccb6c5430ca8a725
Aryan Chopra <chopraryan@gmail.com>
2021-10-25 22:17:49 +0530

    Tailwind + Source maps added

---
+++ src/utils.ts
@@ -0,0 +1,5 @@
+export const CLIENT_ID: string = "f9c23324a99a4d24be742d3953c46b7a";
+export const CLIENT_SECRET: string = "047c5a55ee634c1fb11e8579386815a3";
+
+export const BASE_URL: string = "https://accounts.spotify.com/";
+export const REDIRECT_URI: string = "http://localhost:3000/callback";
