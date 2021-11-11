1d25c92c15f4d96d17d5c6af45fe113b25001a84
Aryan Chopra <chopraryan@gmail.com>
2021-11-11 15:31:53 +0530

    refactor

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-const tailwindcss = require("tailwindcss");
-module.exports = {
-  //   plugins: ["postcss-preset-env", tailwindcss],
-  plugins: [
-    "postcss-preset-env",
-    require("tailwindcss"),
-    require("autoprefixer"),
-  ],
-};
