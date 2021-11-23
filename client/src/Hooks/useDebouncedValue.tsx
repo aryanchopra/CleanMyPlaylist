f3fb2b32c9ba7baca9bd6dedfd78c138ea6d6952
Aryan Chopra <chopraryan@gmail.com>
2021-11-24 01:03:51 +0530

    Debounced Search bar

---
+++ client/src/Hooks/useDebouncedValue.tsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+
+export function useDebouncedValue<T>(input: T, time = 500) {
+  const [debouncedValue, setDebouncedValue] = useState(input);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setDebouncedValue(input);
+    }, time);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [input, time]);
+
+  return debouncedValue;
+}
